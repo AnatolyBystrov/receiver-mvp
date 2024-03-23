336bb77402a1af75892a4d03b5f1ce823bfa259e
Anatoly <anatolyavbme@gmail.com>
2024-03-23 12:09:48 +0200

    initiated commit

---
+++ src/main/java/il/receiver/MvpReceiverApplication.java
@@ -0,0 +1,13 @@
+package il.receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpReceiverApplication.class, args);
+	}
+
+}
