2bf922195db6e6d1df29de1253198a35b3818e1e
SemionRutshtein <0senja0@gmail.com>
2024-04-30 22:43:12 +0300

    fixes

--- src/main/java/il/receiver/external/TogoApiExternalServiceImpl.java
+++ src/main/java/il/receiver/external/TogoApiExternalServiceImpl.java
@@ -30,25 +30,20 @@ public class TogoApiExternalServiceImpl implements TogoApiService {
     private String baseURL;
 
     @Override
-    public CarsV2Response receiveCardsData(Integer projectType, String cars, Integer vType, String licencePlate) {
-                return null;
-    }
-
-    @Override
     public FreeVFlatResponse receiveFreeVehicles(FreeFlatInput freeFlatInput) {
 
         //TODO:: check input data
         String currentEpUrl = "/FreeVehicles/vFlat";
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(prepareUrl(baseURL, currentEpUrl))
-                .queryParam("longitude",freeFlatInput.getLongitude())
-                .queryParam("latitude",freeFlatInput.getLatitude())
-                .queryParam("startDate",freeFlatInput.getStartDate())
-                .queryParam("endDate",freeFlatInput.getEndDate());
+                .queryParam("longitude", freeFlatInput.getLongitude())
+                .queryParam("latitude", freeFlatInput.getLatitude())
+                .queryParam("startDate", freeFlatInput.getStartDate())
+                .queryParam("endDate", freeFlatInput.getEndDate());
 
         String uriString = builder.build().encode().toUriString();
 
-        ResponseEntity<FreeVFlatResponse> responseEntity = restTemplate.getForEntity(uriString, FreeVFlatResponse.class );
+        ResponseEntity<FreeVFlatResponse> responseEntity = restTemplate.getForEntity(uriString, FreeVFlatResponse.class);
         return responseEntity.getBody();
 
 
@@ -62,14 +57,14 @@ public class TogoApiExternalServiceImpl implements TogoApiService {
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
                 .queryParam("startDate", freeVehiclesV2Input.getStartDate())
-                .queryParam("endDate",freeVehiclesV2Input.getEndDate())
-                .queryParam("zoom",freeVehiclesV2Input.getZoom())
-                .queryParam("name",freeVehiclesV2Input.getVType())
-                .queryParam("vType",freeVehiclesV2Input.getVType())
-                .queryParam("categoryId",freeVehiclesV2Input.getCategoryId())
-                .queryParam("longitude",freeVehiclesV2Input.getLongitude())
-                .queryParam("latitude",freeVehiclesV2Input.getLatitude())
-                .queryParam("requestType",freeVehiclesV2Input.getRequestType());
+                .queryParam("endDate", freeVehiclesV2Input.getEndDate())
+                .queryParam("zoom", freeVehiclesV2Input.getZoom())
+                .queryParam("name", freeVehiclesV2Input.getVType())
+                .queryParam("vType", freeVehiclesV2Input.getVType())
+                .queryParam("categoryId", freeVehiclesV2Input.getCategoryId())
+                .queryParam("longitude", freeVehiclesV2Input.getLongitude())
+                .queryParam("latitude", freeVehiclesV2Input.getLatitude())
+                .queryParam("requestType", freeVehiclesV2Input.getRequestType());
 
         String uriString = builder.build().encode().toUriString();
         ResponseEntity<FreeVehiclesV2Response> responseEntity = restTemplate.getForEntity(uriString, FreeVehiclesV2Response.class);
@@ -81,69 +76,76 @@ public class TogoApiExternalServiceImpl implements TogoApiService {
         String baseUrl = "https://autotelpublicapiprod.gototech.co/api/Vehicles/FreeParking";
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
                 .queryParam("latitude", freeParkingInput.getLatitude())
-                .queryParam("longitude",freeParkingInput.getLongitude())
-                .queryParam("modalityId",freeParkingInput.getModalityId())
-                .queryParam("radiusInKilometer",freeParkingInput.getRadiusInKilometer())
-                .queryParam("vType",freeParkingInput.getVType());
+                .queryParam("longitude", freeParkingInput.getLongitude())
+                .queryParam("modalityId", freeParkingInput.getModalityId())
+                .queryParam("radiusInKilometer", freeParkingInput.getRadiusInKilometer())
+                .queryParam("vType", freeParkingInput.getVType());
         String uriString = builder.build().encode().toUriString();
-        ResponseEntity<FreeParkingResponse>responseEntity = restTemplate.getForEntity(uriString,FreeParkingResponse.class);
+        ResponseEntity<FreeParkingResponse> responseEntity = restTemplate.getForEntity(uriString, FreeParkingResponse.class);
         return responseEntity.getBody();
     }
 
     @Override
     public GetInfoUnicResponse receiveUnicCarInfo(GetInfoUnicInput getInfoUnicInput) {
         String baseUrl = "https://autotelpublicapiprod.gototech.co/api/Vehicles/GetInfo";
-        UriComponentsBuilder builder =UriComponentsBuilder.fromHttpUrl(baseUrl)
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
                 .queryParam("projectType", getInfoUnicInput.getProjectType())
                 .queryParam("value", getInfoUnicInput.getValue())
                 .queryParam("externalLicensePlate", getInfoUnicInput.getExternalLicensePlate())
-                .queryParam("vType",getInfoUnicInput.getVType());
-        String uriString =builder.build().encode().toUriString();
-        ResponseEntity<GetInfoUnicResponse>responseEntity =restTemplate.getForEntity(uriString,GetInfoUnicResponse.class);
+                .queryParam("vType", getInfoUnicInput.getVType());
+        String uriString = builder.build().encode().toUriString();
+        ResponseEntity<GetInfoUnicResponse> responseEntity = restTemplate.getForEntity(uriString, GetInfoUnicResponse.class);
         return responseEntity.getBody();
 
     }
+
     @Override
     public GetParkingsResponse receiveUnicCarInfo(GetParkingsResponse getParkingsResponse) {
-            String baseUrl = "https://autotelpublicapiprod.gototech.co/api/FreeVehicles/Parkings";
-            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
-                    .queryParam(" id", getParkingsResponse.getId())
-                    .queryParam("address",getParkingsResponse.getAddress())
-                    .queryParam("latitude",getParkingsResponse.getLatitude())
-                    .queryParam("longitude",getParkingsResponse.getLongitude())
-                    .queryParam("fleetType",getParkingsResponse.getFleetType())
-                    .queryParam("fleetTypeText",getParkingsResponse.getFleetTypeText())
-                    .queryParam("status", getParkingsResponse.getStatus())
-                    .queryParam("capacity", getParkingsResponse.getCapacity())
-                    .queryParam("isElectric", getParkingsResponse.getIsElectric())
-                    .queryParam("notes",getParkingsResponse.getNotes())
-                    .queryParam("modality",getParkingsResponse.getModality());
-            String uriString =builder.build().encode().toUriString();
-            ResponseEntity<GetParkingsResponse>responseEntity = restTemplate.getForEntity(uriString,GetParkingsResponse.class);
-            return responseEntity.getBody();
-        }
+        String baseUrl = "https://autotelpublicapiprod.gototech.co/api/FreeVehicles/Parkings";
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
+                .queryParam(" id", getParkingsResponse.getId())
+                .queryParam("address", getParkingsResponse.getAddress())
+                .queryParam("latitude", getParkingsResponse.getLatitude())
+                .queryParam("longitude", getParkingsResponse.getLongitude())
+                .queryParam("fleetType", getParkingsResponse.getFleetType())
+                .queryParam("fleetTypeText", getParkingsResponse.getFleetTypeText())
+                .queryParam("status", getParkingsResponse.getStatus())
+                .queryParam("capacity", getParkingsResponse.getCapacity())
+                .queryParam("isElectric", getParkingsResponse.getIsElectric())
+                .queryParam("notes", getParkingsResponse.getNotes())
+                .queryParam("modality", getParkingsResponse.getModality());
+        String uriString = builder.build().encode().toUriString();
+        ResponseEntity<GetParkingsResponse> responseEntity = restTemplate.getForEntity(uriString, GetParkingsResponse.class);
+        return responseEntity.getBody();
+    }
 
-        @Override
-        public SearchA2ASingleInput getSingleA2A(SearchA2ASingleInput searchA2ASingleInput){
+    @Override
+    public SearchA2ASingleInput getSingleA2A(SearchA2ASingleInput searchA2ASingleInput) {
         String baseUrl = "https://autotelpublicapiprod.gototech.co/api/FreeVehicles/SearchA2ASingle";
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
-                .queryParam("startDate",searchA2ASingleInput.getStartDate())
-                .queryParam("endDate",searchA2ASingleInput.getEndDate())
-                .queryParam("vehicleId",searchA2ASingleInput.getVehicleId())
-                .queryParam("categoryId",searchA2ASingleInput.getCategoryId())
-                .queryParam("longitude",searchA2ASingleInput.getLongitude())
-                .queryParam("latitude",searchA2ASingleInput.getLatitude())
-                .queryParam("searchSource",searchA2ASingleInput.getSearchSource());
-            String uriString =builder.build().encode().toUriString();
-            ResponseEntity<SearchA2ASingleInput> responseEntity= restTemplate.getForEntity(uriString,SearchA2ASingleInput.class);
-            return responseEntity.getBody();
-        }
+                .queryParam("startDate", searchA2ASingleInput.getStartDate())
+                .queryParam("endDate", searchA2ASingleInput.getEndDate())
+                .queryParam("vehicleId", searchA2ASingleInput.getVehicleId())
+                .queryParam("categoryId", searchA2ASingleInput.getCategoryId())
+                .queryParam("longitude", searchA2ASingleInput.getLongitude())
+                .queryParam("latitude", searchA2ASingleInput.getLatitude())
+                .queryParam("searchSource", searchA2ASingleInput.getSearchSource());
+        String uriString = builder.build().encode().toUriString();
+        ResponseEntity<SearchA2ASingleInput> responseEntity = restTemplate.getForEntity(uriString, SearchA2ASingleInput.class);
+        return responseEntity.getBody();
+    }
+
+
+    @Override
+    public CarsV2Response receiveCardsData(Integer projectType, String cars, Integer vType, String licencePlate) {
+        return null;
+    }
 
 
     private String prepareUrl(String baseURL, String currentEpUrl) {
         //TODO:: check what will work for us
-        return String.format("%s %s", baseURL , currentEpUrl);
+        return String.format("%s %s", baseURL, currentEpUrl);
 //        return  baseURL+currentEpUrl;
     }
 
-    }
+}
